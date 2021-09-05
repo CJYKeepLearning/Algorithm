4cd851559991bcf4a9c2f51b47f624c3fc0933f9
cjy <1292585150@qq.com>
2021-09-05 20:18:30 +0800

    LC470拒绝采样

---
+++ leetcode/src/LC_470.java
@@ -0,0 +1,15 @@
+public class LC_470 {
+    public int rand10() {
+        int row, col, idx;
+        do {
+            row = rand7();
+            col = rand7();
+            idx = col + (row - 1) * 7;
+        } while (idx > 40);
+        return 1 + (idx - 1) % 10;
+    }
+
+    private int rand7() {
+        return (int) (Math.random()*7);
+    }
+}
--- leetcode/src/Test.java
+++ leetcode/src/Test.java
@@ -5,13 +5,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {1,3,5,7,2,4,6,8};
-        int k = 4;
-        IGC_17_14 test = new IGC_17_14();
-        int[] ans = test.smallestKHeapSort(arr,k);
-        for (int i:ans
-             ) {
-            System.out.print(i+" ");
-        }
+        int n =2;
+        System.out.println();
     }
 }

bc99dbf9d9dc3426c7323d1f9d7e46a515c7f069
cjy <1292585150@qq.com>
2022-03-27 08:23:10 +0800

    git commit -m "LC_114"

--- leetcode/src/LC_114.java
+++ leetcode/src/LC_114.java
@@ -1,3 +1,28 @@
 public class LC_114 {
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
 
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+        root.right = left;
+
+        TreeNode p = root;
+        while(p.right != null){
+            p = p.right;
+        }
+        p.right = right;
+    }
+    TreeNode seekRight(TreeNode root){
+        TreeNode res = root;
+        while(res.right != null){
+            res = res.right;
+        }
+        return res;
+    }
 }

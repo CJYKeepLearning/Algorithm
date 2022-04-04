049eebb38b281c46685540ac7e9c75f1bb46ef63
cjy <1292585150@qq.com>
2022-04-04 14:51:17 +0800

    LC_797

---
+++ leetcode/src/LC_797.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LC_797 {
+    List<List<Integer>> res = new LinkedList<>();
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        LinkedList<Integer> path = new LinkedList<>();
+        traverse(graph,0,path);
+        return res;
+    }
+
+    private void traverse(int[][] graph, int s, LinkedList<Integer> path) {
+        path.addLast(s);
+        int n = graph.length;
+        if (s == n-1){
+            res.add(new LinkedList<>(path));
+            path.removeLast();
+            return;
+        }
+        for (int v : graph[s]){
+            traverse(graph,v,path);
+        }
+        path.removeLast();
+    }
+}

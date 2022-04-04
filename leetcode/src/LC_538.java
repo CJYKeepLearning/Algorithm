0aed3b8c28b91c5cd4b82fbd3735fb4847f12f54
cjy <1292585150@qq.com>
2022-04-04 14:50:47 +0800

    LC_538

--- leetcode/src/LC_538.java
+++ leetcode/src/LC_538.java
@@ -3,15 +3,17 @@ public class LC_538 {
 
     }
     public static int sum = 0;
-    TreeNode converBST(TreeNode root){
-        traverse(root);
+    TreeNode convertBST(TreeNode root){
+        convert(root);
         return root;
     }
-    public static  void traverse(TreeNode root){
-       if (root==null) return;
-        traverse(root.right);
-        root.val = root.val+sum;
-        sum = root.val;
-        traverse(root.left);
+    int preSum = 0;
+    private void convert(TreeNode root) {
+        if (root == null)
+            return;
+        convert(root.right);
+        root.val = preSum + root.val;
+        preSum = root.val;
+        convert(root.left);
     }
 }

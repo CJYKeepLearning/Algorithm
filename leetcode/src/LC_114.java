61368ca0d42288608b0d5ae114e830fcf387002d
cjy <1292585150@qq.com>
2022-04-04 14:50:18 +0800

    LC_114

--- leetcode/src/LC_114.java
+++ leetcode/src/LC_114.java
@@ -1,28 +1,18 @@
 public class LC_114 {
     public void flatten(TreeNode root) {
-        if(root == null){
+        if (root == null){
             return;
         }
-        flatten(root.left);
-        flatten(root.right);
-
         TreeNode left = root.left;
         TreeNode right = root.right;
-
+        flatten(root.left);
+        flatten(root.right);
         root.left = null;
         root.right = left;
-
-        TreeNode p = root;
-        while(p.right != null){
+        TreeNode p = left;
+        while (p.right != null){
             p = p.right;
         }
         p.right = right;
-    }
-    TreeNode seekRight(TreeNode root){
-        TreeNode res = root;
-        while(res.right != null){
-            res = res.right;
-        }
-        return res;
     }
 }

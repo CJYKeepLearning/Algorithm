8e90242bf67fb411ebe0c1ad9b2bde1b088433c3
cjy <1292585150@qq.com>
2022-04-04 14:50:58 +0800

    LC_654

--- leetcode/src/LC_654.java
+++ leetcode/src/LC_654.java
@@ -1,6 +1,27 @@
 public class LC_654 {
     public static void main(String[] args) {
-        int[] nums = {1,2,3};
+
+    }
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return constructMax(nums,0,nums.length-1);
     }
 
+    private TreeNode constructMax(int[] nums, int left, int right) {
+        if (left > right){
+            return null;
+        }
+        int idx = 0, max = 0;
+        for (int i = left;i<=right;i++){
+            if (max < nums[i]){
+                max = nums[i];
+                idx = i;
+            }
+        }
+        TreeNode root = new TreeNode(max);
+        TreeNode l = constructMax(nums,left,idx-1);
+        root.left = l;
+        TreeNode r = constructMax(nums,idx+1,right);
+        root.right = r;
+        return root;
+    }
 }
